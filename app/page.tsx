f0e23b0c160915632c0e0825b5b2d1fd3953f95a
iisce <iscedigitalconcept@gmail.com>
2025-09-30 17:25:22 +0100

    feat: add NFCChecker component to check NFC capability and integrate it into HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -7,37 +7,41 @@
 // import StoreManagement from "@/components/home/StoreManagement";
 // import AccountSettingsList from "@/components/home/AccountSettingsList";
 
-import AccountSettingsList from "@/components/pages/home/account-settings";
-import ConnectManagement from "@/components/pages/home/contact-management";
-import WalletCard from "@/components/pages/home/contact-wallet";
-import DevicesCard from "@/components/pages/home/device-section";
-import EventCard from "@/components/pages/home/event-card";
-import ProfileHeader from "@/components/pages/home/profile-header";
-import PromoBanner from "@/components/pages/home/promo-banner";
-import StoreManagement from "@/components/pages/home/store-management";
+import AccountSettingsList from '@/components/pages/home/account-settings';
+import ConnectManagement from '@/components/pages/home/contact-management';
+import WalletCard from '@/components/pages/home/contact-wallet';
+import DevicesCard from '@/components/pages/home/device-section';
+import EventCard from '@/components/pages/home/event-card';
+import ProfileHeader from '@/components/pages/home/profile-header';
+import PromoBanner from '@/components/pages/home/promo-banner';
+import StoreManagement from '@/components/pages/home/store-management';
+import NFCChecker from '@/components/shared/nfc-checker';
 
 export default function HomePage() {
-  return (
-    <main className="bg-black text-white min-h-screen">
-      <section className="">
-        <ProfileHeader />
-      </section>
-      <section className="p-4 space-y-5">
-        <PromoBanner />
-        <EventCard />
-      </section>
-      <section className="p-4 space-y-10">
-        <DevicesCard />
-        <WalletCard />
-      </section>
-      <section className="p-4 space-y-10">
-        <ConnectManagement />
-        <StoreManagement />
-      </section>
+	return (
+		<main className='bg-black text-white min-h-screen'>
+			<section className=''>
+				<ProfileHeader />
+			</section>
+			<section className=''>
+				<NFCChecker />
+			</section>
+			<section className='p-4 space-y-5'>
+				<PromoBanner />
+				<EventCard />
+			</section>
+			<section className='p-4 space-y-10'>
+				<DevicesCard />
+				<WalletCard />
+			</section>
+			<section className='p-4 space-y-10'>
+				<ConnectManagement />
+				<StoreManagement />
+			</section>
 
-      <section className="p-4">
-        <AccountSettingsList />
-      </section>
-    </main>
-  );
+			<section className='p-4'>
+				<AccountSettingsList />
+			</section>
+		</main>
+	);
 }
