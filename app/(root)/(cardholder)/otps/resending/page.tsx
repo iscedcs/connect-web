20e0214a7ec8fa606ee8f3eb3ac9e671c7299c4c
fusco <emekaignatius5@gmail.com>
2025-12-09 15:19:19 +0100

    chore: removed otps pages and added account settings to routes

--- app/(root)/(cardholder)/otps/resending/page.tsx
+++
@@ -1,5 +0,0 @@
-import OtpScreen from "@/components/cardholder/otp/otp-screen";
-
-export default function Page() {
-  return <OtpScreen state="resending" />;
-}
--- app/(root)/(cardholder)/otps/success/page.tsx
+++
@@ -1,5 +0,0 @@
-import OtpSucesssSection from "@/components/pages/cardholder/home/otpsucesssection";
-
-export default function Page() {
-  return <OtpSucesssSection />;
-}
