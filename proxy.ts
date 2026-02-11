2cde19b8be22028dfaa639b0ef8d1930776e4802
iisce <iscedigitalconcept@gmail.com>
2026-02-11 02:42:48 +0100

    migrated to next 16

--- proxy.ts
+++ proxy.ts
@@ -12,7 +12,7 @@ function isProtectedPath(pathname: string) {
 	);
 }
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 
 	// Allow static/assets/callback/etc.
